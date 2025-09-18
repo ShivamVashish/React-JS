ae60a14683eece708cfb042f009d657e813a2ff3
Shivam Sharma <sharma.shivam2384@gmail.com>
2025-09-18 10:37:36 +0530

    update class o1

--- class 02/Notesscript.js
+++ class 02/Notesscript.js
@@ -10,8 +10,11 @@ const h2 = React.createElement('h2',{className:'sub-heading',id:'Sub-title'},'He
 // props (isme do chez show ho rhi h ek to children:'hello react js or className:sub-heading)
 // page pr show krane ke liye (iske liye React DOM use krege react m koi trika nhi hota)
 // phle hme html m div create krke id="root" dete h
-const root = ReactDOM.createRoot(document.querySelector('#root'))//ye btana hota h ki iske andr create krdo 
-root.render(h2) // Render is a presentation tool to show the output your data in your display and render is
+
+// const root = ReactDOM.createRoot(document.querySelector('#root'))//ye btana hota h ki iske andr create krdo 
+// root.render(h2)
+
+//  // Render is a presentation tool to show the output your data in your display and render is
 //used to create dom element and interconnected to each other
 
 // React.createElement krte h to object create hota or phir uss object ko hm render ko de dete h render method
@@ -22,3 +25,23 @@ root.render(h2) // Render is a presentation tool to show the output your data in
 // const h3 = document.createElement('h3') /// ye bht sari properties h isme or bht space leta h 
 // h3.innerText = 'Hello Js'
 // document.querySelector('#root').append(h3) // ye h javascript ki help se create krna ek element ko 
+
+// ek ke andr ek element create krna 
+// const container = React.createElement('div',{className:'container',id:'main-container'},
+// [React.createElement('section',{key:1'},
+// [React.createElement('p',{key:2},'Hello Shivam')]
+// )]
+// )
+// const root = ReactDOM.createRoot(document.querySelector('#root'))
+// root.render(container)
+
+// Javascript se ye code h pure or nahi isme id de rkhi h but usme id class content ek sath de skte h 
+
+// const rootdiv =  document.querySelector('#root')
+// const innerdiv = document.createElement('div')
+// const section = document.createElement('section')
+// const p = document.createElement('p')
+// p.innerText = 'Hello Coder Army'
+// section.append(p)
+// innerdiv.append(section)
+// rootdiv.append(innerdiv)
--- class 02/mainscript.js
+++ class 02/mainscript.js
@@ -4,12 +4,38 @@
 // React Element : A React element is a plain JavaScript object. And it's a lightweight, and it 
 // can take multiple attributes, type, props, and it's a blueprint of UI, not a DOM
 
-// React Component : A React Component is a function or Class it main work is create a UI and its return a react
+// React Component : A React Component is a function or Class it main work is create a UI and its return a 
 // react element and It takes props as input to receive data(child) from a parent component.
 
-// Why $$typeof is Used : It is used for security purpose hacking na ho
-// $$typeof: Symbol.for('react.element'),
-// Create Element and Data Providing use React JS
-const h2 = React.createElement('h2',{className:'sub-heading',id:'Sub-title'},'Hello React JS')
+// Why $$typeof is Used : It is used for security purpose (koi bhi maleshish code api ke throw na bhej ske)
+// $$typeof: Symbol.for('react.element'), symbol transfer hi nhi hone dega
+// Create Element and Data Providing use React JS(Single)
+
+// const h2 = React.createElement('h2',{className:'sub-heading',id:'Sub-title'},'Hello React JS')
+// const root = ReactDOM.createRoot(document.querySelector('#root'))
+// root.render(h2)
+
+// Root ke andr ek container ke andr section or section ke andr paragraph(Child Parent)
+//Array pass krege to hi key ki jarurat h vrna nhi
+
+// const container = React.createElement('div',{className:'container',id:'main-container'},
+// [React.createElement('section',{key:1},
+// [React.createElement('p',{key:2},'Hello Shivam')]
+// )])
+// const root = ReactDOM.createRoot(document.querySelector('#root'))
+// root.render(container)
+
+// (Siblings)
+// style set krne ke liye Object crete krkre key value pair m likheg
+const BigContainer = React.createElement('div',{id:'main-contianer', style: { key:1,textAlign:'center'}},[
+    React.createElement('h3',{key:1,id:'heading-3', style: {fontSize:'40px'}},'Form'),
+    // React.createElement('p',{ style: {backgroundColor:"green", fontSize:'20px',fontWeight:'bold'}},'Hello smaller Brother'),
+    React.createElement('img',{ key: 2,style : {width:'100%', maxWidth: '300px',backgroundColor:'Blue',padding:"10px",borderRadius:"20px"}, src:'https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F18h8oyjknd6q0tmkz383.png'}),
+    React.createElement('div',{key:3,id:'input-container'},[
+    React.createElement('label',{key:1, htmlFor: 'username',  },'Username : '),
+    React.createElement('input',{key:2, id: 'username',type:'password', style:{margin: '10px'}}),
+    ]) 
+])
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(h2)
+root.render(BigContainer)
+
