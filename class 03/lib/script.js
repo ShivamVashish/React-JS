9949eff6399c9020c2cd230fff4bef85bde871d0
Shivam Sharma <sharma.shivam2384@gmail.com>
2025-09-21 18:10:40 +0530

    class 03

---
+++ class 03/lib/script.js
@@ -0,0 +1,40 @@
+const BigContainer = React.createElement('div', {
+  id: 'main-contianer',
+  style: {
+    key: 1,
+    textAlign: 'center'
+  }
+}, [React.createElement('h3', {
+  key: 1,
+  id: 'heading-3',
+  style: {
+    fontSize: '40px'
+  }
+}, 'Form'),
+// React.createElement('p',{ style: {backgroundColor:"green", fontSize:'20px',fontWeight:'bold'}},'Hello smaller Brother'),
+React.createElement('img', {
+  key: 2,
+  style: {
+    width: '100%',
+    maxWidth: '300px',
+    backgroundColor: 'Blue',
+    padding: "10px",
+    borderRadius: "20px"
+  },
+  src: 'https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F18h8oyjknd6q0tmkz383.png'
+}), React.createElement('div', {
+  key: 3,
+  id: 'input-container'
+}, [React.createElement('label', {
+  key: 1,
+  htmlFor: 'username'
+}, 'Username : '), React.createElement('input', {
+  key: 2,
+  id: 'username',
+  type: 'password',
+  style: {
+    margin: '10px'
+  }
+})])]);
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(BigContainer);
---
+++ class 03/text.js
@@ -0,0 +1,27 @@
+// Uncaught SyntaxError: Unexpected token '<' iss syntax ji js smj nhi paa rha h iske liye use krege
+// bebel jo h ye jsx ko bhi javascript m convert kr deta h ye new js ko purane js m convert krta h or 
+// jsx ko bhi js m convert krta h phir react.createElement m bdl deta h
+// bebel ka cdn link use krkr km krna hota h agr link htane pr error nhi dega bss balel ki value kuch bhi ho 
+// javscript nhi honi chaiye jo likhi h niche by default javscript hoti h or wo error deti h javscript smj kr 
+// agr hm javascript ki jgah kuch bhi likh denge to code run nhi hogi or error to phir ayegi hi ni kyuki
+// browser request send krega hi ni
+
+// Babel : babel is open source javascript compiler and it is used to convert a modern javascrpt to old
+// javascript and Babel is requesting a file and transforming a file and after convert into a react.element
+
+// type="text/babel" iss lgane se script tag kse cl rha h code kya jaddu kia h isme kse chl rha h
+//  ye 
+
+// hme babel ka code lekr hi file create krli 
+
+// jo bhi hmara code tha wo hmne sbhi ka sbhi html wala apne javascript m store kr lia or wo bilkul sy hora h
+// isko hi hm jsx bolte h 
+
+
+// agr hme koi code m kisi or element ki value chaiye to object m store krke deneg asa 
+// const user = 'Shivam Sharma'
+// const h2 = <h2>Hello {user}</h2> object ke andr koi bhi logic run kr skte h
+
+// const root = ReactDOM.createRoot(document.querySelector('#root'))
+// root.render(h2)
+// React undefined,null,true,false ye sb show nhi krta 
